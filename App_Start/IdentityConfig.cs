f93a7edce1ed1cceb02f2f932a05f7088e3349f4
Dmitry Shepelev <dmitry.shepelev@live.ru>
2014-08-01 20:36:23 +0300

    Auth Bug

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -29,10 +29,7 @@ namespace SocialNetwork
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
             };
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             // You can write your own provider and plug in here.
