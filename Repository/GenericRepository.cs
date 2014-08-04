3d9610dc82f59228ca9a20b2bf7087f7ee17f456
Dmitry Shepelev <dmitry.shepelev@live.ru>
2014-08-04 13:13:15 +0300

    Added Repositories :100:

---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Repository
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        internal ApplicationDbContext dbContext;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = dbContext.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (var includeProperty in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();
+        }
+
+        public virtual TEntity GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entity = dbSet.Find(id);
+            Delete(entity);
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            if (dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+            dbSet.Remove(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            dbSet.Attach(entity);
+            dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Repository/Repositories.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SocialNetwork.Models;
+
+namespace SocialNetwork.Repository
+{
+    public class Repositories : IDisposable
+    {
+        private ApplicationDbContext dbContext = new ApplicationDbContext();
+        private GenericRepository<UserTaskModel> userTaskRepository;
+        private GenericRepository<SolutionModel> solutionRepository;
+        private GenericRepository<TagModel> tagRepository;
+        private GenericRepository<CategoryModel> categoryRepository;
+        private GenericRepository<LikeModel> likeRepository;
+        private GenericRepository<CommentModel> commentRepository;
+        private GenericRepository<UserProposedSolutionModel> userProposedSolutionRepository;
+
+        public GenericRepository<UserTaskModel> UserTaskRepository
+        {
+            get
+            {
+                if (this.userTaskRepository == null)
+                {
+                    this.userTaskRepository = new GenericRepository<UserTaskModel>(dbContext);
+                }
+                return userTaskRepository;
+            }
+        }
+
+        public GenericRepository<SolutionModel> SolutionRepository
+        {
+            get
+            {
+                if (this.solutionRepository == null)
+                {
+                    this.solutionRepository = new GenericRepository<SolutionModel>(dbContext);
+                }
+                return solutionRepository;
+            }
+        }
+
+        public GenericRepository<TagModel> TagRepository
+        {
+            get
+            {
+                if (this.tagRepository == null)
+                {
+                    this.tagRepository = new GenericRepository<TagModel>(dbContext);
+                }
+                return tagRepository;
+            }
+        }
+
+        public GenericRepository<CategoryModel> CategoryRepository
+        {
+            get
+            {
+                if (this.categoryRepository == null)
+                {
+                    this.categoryRepository = new GenericRepository<CategoryModel>(dbContext);
+                }
+                return categoryRepository;
+            }
+        }
+
+        public GenericRepository<LikeModel> LikeRepository
+        {
+            get
+            {
+                if (this.likeRepository == null)
+                {
+                    this.likeRepository = new GenericRepository<LikeModel>(dbContext);
+                }
+                return likeRepository;
+            }
+        }
+
+        public GenericRepository<CommentModel> CommentRepository
+        {
+            get
+            {
+                if (this.commentRepository == null)
+                {
+                    this.commentRepository = new GenericRepository<CommentModel>(dbContext);
+                }
+                return commentRepository;
+            }
+        }
+
+        public GenericRepository<UserProposedSolutionModel> UserProposedSolutionRepository
+        {
+            get
+            {
+                if (this.userProposedSolutionRepository == null)
+                {
+                    this.userProposedSolutionRepository = new GenericRepository<UserProposedSolutionModel>(dbContext);
+                }
+                return userProposedSolutionRepository;
+            }
+        }
+
+        public void Save()
+        {
+            dbContext.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    dbContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
