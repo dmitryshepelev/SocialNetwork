ae8d652227817125ff26780a29d4575ad0364ad6
PolinaRuban <polli.simple@gmail.com>
2014-08-12 21:56:08 +0300

    register and reset (c/temp, not mail)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -126,8 +126,7 @@ namespace SocialNetwork.Controllers
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
+                    return View("DisplayEmail");
                 }
                 else
                 {
